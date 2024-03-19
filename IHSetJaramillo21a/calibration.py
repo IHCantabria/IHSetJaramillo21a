447a7550d0dbd4fc7c9e50b56590cd8ce8f31f3d
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-03-19 12:39:16 +0100

    Build pack

--- IHSetJaramillo21a/calibration.py
+++ IHSetJaramillo21a/calibration.py
@@ -138,8 +138,9 @@ class cal_Jaramillo21a(object):
 
         # Validation
         idx = np.where((self.time_obs < self.start_date) | (self.time_obs > self.end_date))
+        self.idx_validation_obs = idx
         mkIdx = np.vectorize(lambda t: np.argmin(np.abs(self.time[self.idx_validation] - t)))
-        self.idx_validation_obs = mkIdx(self.time_obs[idx])
+        self.idx_validation_for_obs = mkIdx(self.time_obs[idx])
 
 
         
