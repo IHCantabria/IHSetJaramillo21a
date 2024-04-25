ba804819908b9ddeb6d9e09c58973a2c2a93e9f7
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-04-25 21:23:40 +0200

    Build pack

--- IHSetJaramillo21a/calibration.py
+++ IHSetJaramillo21a/calibration.py
@@ -120,6 +120,10 @@ class cal_Jaramillo21a(object):
         """
         Split the data into calibration and validation datasets.
         """
+        ii = np.where(self.time>=self.start_date)[0][0]
+        self.P = self.E[ii:]
+        self.time = self.time[ii:]
+
         idx = np.where((self.time < self.start_date) | (self.time > self.end_date))
         self.idx_validation = idx
         
@@ -138,9 +142,15 @@ class cal_Jaramillo21a(object):
 
         # Validation
         idx = np.where((self.time_obs < self.start_date) | (self.time_obs > self.end_date))
-        self.idx_validation_obs = idx
-        mkIdx = np.vectorize(lambda t: np.argmin(np.abs(self.time[self.idx_validation] - t)))
-        self.idx_validation_for_obs = mkIdx(self.time_obs[idx])
+        self.idx_validation_obs = idx[0]
+        if len(self.idx_validation)>0:
+            mkIdx = np.vectorize(lambda t: np.argmin(np.abs(self.time[self.idx_validation] - t)))
+            if len(self.idx_validation_obs)>0:
+                self.idx_validation_for_obs = mkIdx(self.time_obs[idx])
+            else:
+                self.idx_validation_for_obs = []
+        else:
+            self.idx_validation_for_obs = []
 
 
         
